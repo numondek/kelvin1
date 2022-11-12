05ca9cb3f33cc6aaaef9a3c2833b1142c30a0734
NUMONDE <Numondekelvin@gmail.com>
2022-11-12 16:00:57 +0100

    commit

--- lib/data/api/user_transaction.dart
+++ lib/data/api/user_transaction.dart
@@ -1,3 +1,30 @@
+import 'dart:convert';
+
+import 'package:d8er1/data/model/user_info.dart';
+import 'package:http/http.dart' as http;
+
 class UserTransactions{
 
+
+  fetchTransaction({String? token}) async {
+    Uri url = Uri.parse('https://d8er-app.com/api/transactions');
+    var response = await http.get(url,
+      headers: {"Authorization": "Bearer $token"},
+    );
+    try {
+      if (response.statusCode == 200) {
+        var jsonData = jsonDecode(response.body);
+        print(jsonData);
+        return UserTransactions.fromJson(jsonData);
+      }else if (response.statusCode == 404) {
+        var jsonData = jsonDecode(response.body);
+        print(jsonData);
+        String er = jsonData['message'];
+        throw Exception(er);
+      }
+    } catch (e) {
+      final data = e.toString().substring(11);
+      return UserDetail.withError(data);
+    }
+  }
 }
--- lib/data/model/user_transaction.dart
+++ lib/data/model/user_transaction.dart
@@ -1,3 +1,54 @@
-class UserTransaction{
+class UserTransaction {
+  String? username;
+  String? dataAmount;
+  int? transactionId;
+  bool? credit;
+  bool? toWallet;
+  DateTime? dateTime;
+  String? sentTo;
+  String? receivedFrom;
+  String? error;
 
+  UserTransaction(
+      {this.username,
+      this.dateTime,
+      this.dataAmount,
+      this.credit,
+      this.transactionId,
+      this.toWallet,
+      this.receivedFrom,
+      this.sentTo});
+
+  UserTransaction.withError(String errorMessage) {
+    error = errorMessage;
+  }
+
+  UserTransaction.fromJson(Map<String, dynamic> json,) {
+
+    error = json['detail'];
+    username = json['First_Name'] ??= '';
+    transactionId = json['Last_Name'] ??= '';
+    credit = json['Gender'] ??= '';
+    dataAmount = json['City'] ??= '';
+    toWallet = json['Phone_number'] ??= '';
+    receivedFrom = json['Data_balance'];
+    sentTo = json['Mobile_network'];
+    dateTime = json['Mobile_network'];
+
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['First_Name'] = username;
+    data['Last_Name'] = transactionId;
+    data['Gender'] = credit;
+    data['City'] = dataAmount;
+    data['Phone_number'] = toWallet;
+    data['Data_balance'] = receivedFrom;
+    data['Mobile_network'] = sentTo;
+    data['Mobile_network'] = dateTime;
+
+
+    return data;
+  }
 }
